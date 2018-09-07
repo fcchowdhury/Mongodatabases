96a37d231716d0f7b315c4cbe35158daa1fe79e6
fcchowdhury <chowdhurybabar@gmail.com>
2018-09-06 22:09:50 -0400

    my mongodatabase source

---
+++ src/ConnectMongo/User.java
@@ -0,0 +1,54 @@
+package ConnectMongo;
+
+public class User {
+
+    String studentName;
+
+
+
+    String studentID;
+    String studentDOB;
+
+    public User(){
+
+    }
+
+    public User(String studentName, String studentID){
+        this.studentName=studentName;
+        this.studentID=studentID;
+    }
+    public User(String studentName, String studentID,String studentDOB){
+        this.studentName=studentName;
+        this.studentID=studentID;
+        this.studentDOB = studentDOB;
+    }
+
+    public User(String studentID){
+
+        this.studentID=studentID;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getStudentDOB() {
+        return studentDOB;
+    }
+
+    public void setStudentDOB(String studentDOB) {
+        this.studentDOB = studentDOB;
+    }
+}
